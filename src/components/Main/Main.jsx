06613ba36961d869c06d30f808c0ffcbc74274f2
jyoti0828 <157128619+jyoti0828@users.noreply.github.com>
2024-08-12 02:29:22 +0530

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -29,7 +29,7 @@ const Main = () => {
           <>
             <div className="greet">
               <p>
-                <span>Hello, Jyoti&Rohit</span>
+                <span>Hello, Jyoti</span>
               </p>
               <p>How can I help you today?</p>
             </div>
